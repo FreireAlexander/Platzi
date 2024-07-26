bf341b41395fef72be4fe430a84bbb3acb6825e4
zero_asus <zero@ASUS.>
2024-07-26 09:16:16 -0500

    course: Curso Practico de Python: Creacion de un CRUD lesson: Init Course
    author: Zero

---
+++ courses/pythonPracticoCRUD/project/main.py
@@ -0,0 +1,15 @@
+'''
+    This a software for apply a CRUD on a Business related to calculate Taxes
+
+'''
+
+
+def main() -> int:
+
+    print(f"Start Coding")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
